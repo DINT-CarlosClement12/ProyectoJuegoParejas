f0e4a2401b5ba6887abf63ccaf4716f104d2eeea
Carlos Clement Bellido <carlos.clement.bellido@gmail.com>
2019-10-11 20:52:17 +0200

    Added matching cards, fliping and timing.

--- ProyectoJuegoParejas/MainWindow.xaml.cs
+++ ProyectoJuegoParejas/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ProyectoJuegoParejas
 {
@@ -20,17 +21,44 @@ namespace ProyectoJuegoParejas
     /// </summary>
     public partial class MainWindow : Window
     {
+        private struct CardComparer
+        {
+            public TextBlock card1;
+            public TextBlock card2;
+        }
+
+        const int NUM_POSSIBLE_REPETITIONS = 2;
+        const char INTERROGATION_SIGN = 's';
+        Brush DEFAULT_CARD_BRUSH = Brushes.Gainsboro; // change solid color to fade
+
+        CardComparer comparingCards = new CardComparer();
+
         public MainWindow()
         {
             InitializeComponent();
-
-            DrawGame(4);
         }
 
-        private void DrawGame(int columnLength)
+        private void DrawGame(int totalPlayingCards)
         {
-            int halfColumnLength = columnLength / 2;
-            for (int i = 0; i < halfColumnLength; i++)
+            ResetComparison();
+            gameGrid.Children.Clear();
+            int columnLength = (int)Math.Sqrt(totalPlayingCards);
+
+            List<char> randomCharacters = new List<char>();
+            Random rnd = new Random();
+            for (int i = 0; i < columnLength * columnLength / 2; i++)
+            {
+                char actualChar = (char)rnd.Next('A', 'Z');
+                if (randomCharacters.Contains(actualChar))
+                {
+                    i--;
+                    continue;
+                }
+                for (int j = 0; j < NUM_POSSIBLE_REPETITIONS; j++)
+                    randomCharacters.Add(actualChar);
+            }
+
+            for (int i = 0; i < columnLength; i++)
             {
                 RowDefinition rowDefinition = new RowDefinition();
                 rowDefinition.Height = new GridLength(1, GridUnitType.Star);
@@ -40,21 +68,107 @@ namespace ProyectoJuegoParejas
                 columnDefinition.Width = new GridLength(1, GridUnitType.Star);
                 gameGrid.ColumnDefinitions.Add(columnDefinition);
 
-                for (int j = 0; j < halfColumnLength; j++)
+                for (int j = 0; j < columnLength; j++)
                 {
-                    /*Image img = new Image();
-                    img.Source = new BitmapImage(new Uri(Environment.CurrentDirectory + "/angel.png"));
-                    Grid.SetRow(img, i);
-                    Grid.SetColumn(img, j);
-                    gameGrid.Children.Add(img);*/
-                    TextBlock txtBlock = new TextBlock();
-                    txtBlock.FontFamily = new FontFamily("Webdings");
-                    txtBlock.Text = "l";
-                    Grid.SetRow(txtBlock, i);
-                    Grid.SetColumn(txtBlock, j);
-                    gameGrid.Children.Add(txtBlock);
+                    int currentIndexChar = rnd.Next(0,randomCharacters.Count - 1);
+
+                    TextBlock frontCard = new TextBlock
+                    {
+                        FontFamily = new FontFamily("Webdings"),
+                        Tag = randomCharacters[currentIndexChar].ToString(),
+                        Text = INTERROGATION_SIGN.ToString()
+                    };
+
+                    Viewbox viewbox = new Viewbox
+                    {
+                        Child = frontCard
+                    };
+
+                    Border border = new Border
+                    {
+                        Background = DEFAULT_CARD_BRUSH,
+                        Margin = new Thickness(5),
+                        BorderBrush = Brushes.Black,
+                        BorderThickness = new Thickness(5),
+                        CornerRadius = new CornerRadius(5),
+                        Child = viewbox
+                    };
+                    border.MouseDown += FlipCard;
+
+
+                    randomCharacters.RemoveAt(currentIndexChar);
+
+                    Grid.SetRow(border, i);
+                    Grid.SetColumn(border, j);
+
+                    gameGrid.Children.Add(border);
                 }
             }
+        }
+
+        private void FlipCard(object sender, RoutedEventArgs e)
+        {
+            if (comparingCards.card1 == null || comparingCards.card2 == null)
+            {
+                Border selectedObjectBorder = (Border)sender;
+                Viewbox selectedViewbox = (Viewbox)selectedObjectBorder.Child;
+                TextBlock selectedTextBlock = (TextBlock)selectedViewbox.Child;
+
+                selectedObjectBorder.Background = Brushes.White;
+                selectedTextBlock.Text = selectedTextBlock.Tag.ToString();
+
+                if (comparingCards.card1 != null)
+                {
+                    comparingCards.card2 = selectedTextBlock;
+
+                    if (comparingCards.card1.Tag.ToString() != comparingCards.card2.Tag.ToString())
+                    {
+
+                        DispatcherTimer timer = new DispatcherTimer();
+                        timer.IsEnabled = false;
+                        timer.Interval = TimeSpan.FromMilliseconds(1000);
+
+                        int delaySeconds = 2;
+                        timer.Start();
+                        timer.Tick += delegate
+                        {
+                            delaySeconds--;
+                            if (delaySeconds == 0)
+                            {
+                                timer.Stop();
+                                UnflipCards();
+                            }
+                        };
+                    }
+                    else
+                        ResetComparison();
+                }
+                else
+                    comparingCards.card1 = selectedTextBlock;
+            }
+        }
+
+        private void UnflipCards()
+        {
+            comparingCards.card1.Text = INTERROGATION_SIGN.ToString();
+            ((Border)((Viewbox)comparingCards.card1.Parent).Parent).Background = DEFAULT_CARD_BRUSH;
+
+            comparingCards.card2.Text = INTERROGATION_SIGN.ToString();
+            ((Border)((Viewbox)comparingCards.card2.Parent).Parent).Background = DEFAULT_CARD_BRUSH;
+
+            ResetComparison();
+        }
+
+        private void ResetComparison()
+        {
+            comparingCards.card1 = null;
+            comparingCards.card2 = null;
+        }
+
+        private void InitButton_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton radioButtonSelected = radioButtonContainer.Children.Cast<RadioButton>().Single(r => r.IsChecked == true);
+            DrawGame((int)Math.Pow(double.Parse(radioButtonSelected.Tag.ToString()), 2));
         }
     }
 }
