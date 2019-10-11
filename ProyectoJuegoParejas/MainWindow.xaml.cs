4460c2e3784b21e713585c70c00bf7400f146a8e
CarlosClement12 <carlos.clement@iesdoctorbalmis.com>
2019-10-11 14:03:06 +0200

    Ave, Caesar

--- ProyectoJuegoParejas/MainWindow.xaml.cs
+++ ProyectoJuegoParejas/MainWindow.xaml.cs
@@ -23,6 +23,35 @@ namespace ProyectoJuegoParejas
         public MainWindow()
         {
             InitializeComponent();
+
+            DrawGame(4);
+        }
+
+        private void DrawGame(int columnLength)
+        {
+            int halfColumnLength = columnLength / 2;
+            for (int i = 0; i < halfColumnLength; i++)
+            {
+                RowDefinition rowDefinition = new RowDefinition();
+                rowDefinition.Height = new GridLength(1, GridUnitType.Star);
+                gameGrid.RowDefinitions.Add(rowDefinition);
+
+                ColumnDefinition columnDefinition = new ColumnDefinition();
+                columnDefinition.Width = new GridLength(1, GridUnitType.Star);
+                gameGrid.ColumnDefinitions.Add(columnDefinition);
+
+                for (int j = 0; j < halfColumnLength; j++)
+                {
+                    /*Image img = new Image();
+                    img.Source = new BitmapImage(new Uri(Environment.CurrentDirectory + "/angel.png"));
+                    Grid.SetRow(img, i);
+                    Grid.SetColumn(img, j);
+                    gameGrid.Children.Add(img);*/
+                    TextBlock txtBlock = new TextBlock();
+                    txtBlock.FontFamily = 
+                    txtBlock.Text = "c";
+                }
+            }
         }
     }
 }
